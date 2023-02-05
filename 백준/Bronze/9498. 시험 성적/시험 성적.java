1af1f2d654d6c37b6d7fbc806c44010498b770c2
AndJ <117615536+AndJ2@users.noreply.github.com>
2023-02-05 13:23:17 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int num = a/10;
+        
+        switch(num) {
+            case 10:
+                System.out.println("A");
+                break;
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+        }
+    }
+}
