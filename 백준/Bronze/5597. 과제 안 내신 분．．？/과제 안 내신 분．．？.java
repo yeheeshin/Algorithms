1e20f94b861ad7c937b2893406be41614c1da5e5
AndJ <117615536+AndJ2@users.noreply.github.com>
2023-02-19 23:32:03 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int arr[] = new int[30];
+		
+		for(int i=0;i<28;i++) {
+			arr[Integer.parseInt(br.readLine())-1] = 1;
+		}
+		
+		for(int i=0;i<30;i++) {
+			if(arr[i]==0)
+				System.out.println(i+1);
+		}
+	}
+
+}
